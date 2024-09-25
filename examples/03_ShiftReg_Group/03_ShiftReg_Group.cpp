4f73be581336ab02eccb71f5b5ecd8c80b40e9f6
HackinSpock <winchell587matthew@gmail.com>
2024-09-25 18:50:38 -0400

    Added x2 additional steppers to example

--- examples/03_ShiftReg_Group/03_ShiftReg_Group.cpp
+++ examples/03_ShiftReg_Group/03_ShiftReg_Group.cpp
@@ -9,13 +9,16 @@ using namespace TS4;
 Shifty shift;
 
 Stepper s1(&shift, 1, 2);
-Stepper s2(&shift, 3, 4);
+Stepper s3(&shift, 3, 4);
+Stepper s2(&shift, 11, 12);
+Stepper s4(&shift, 9, 10);
+
 
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     // Set the number of bits you have (multiples of 8)
-    shift.setBitCount(8);
+    shift.setBitCount(16);
 
   // Set the clock, data, and latch pins you are using
   // This also sets the pinMode for these pins
@@ -35,27 +38,47 @@ void setup()
         // .setAcceleration(25'000);
         .setMaxSpeed(1'000)
         .setAcceleration(5'000);
+    s3
+        // .setMaxSpeed(10'000)
+        // .setAcceleration(50'000);
+        .setMaxSpeed(1'000)
+        .setAcceleration(5'000);
+
+    s4
+        // .setMaxSpeed(8'000)
+        // .setAcceleration(25'000);
+        .setMaxSpeed(1'000)
+        .setAcceleration(5'000);
     // move two steppers simultaneously (not synchronized)
     s1.moveAbsAsync(1000);
     s2.moveAbsAsync(4000);
+    s3.moveAbsAsync(4000);
+    s4.moveAbsAsync(4000);
 
     // wait until both steppers are done
-    while (s1.isMoving || s2.isMoving)
+    while (s1.isMoving || s2.isMoving || s3.isMoving)
     {
         delay(10);
     }
     delay(100);
 
     // define and move a groups of steppers synchronized
-    StepperGroup g1{s1, s2};
+    //StepperGroup g1{s1, s2, s3, s4};
+    StepperGroup g1{s1, s2, s3, s4};
 
     s1.setTargetAbs(-1000);
     s2.setTargetAbs(-3000);
+    s3.setTargetAbs(-3000);
+    s4.setTargetAbs(-3000);
+    //s3.setTargetAbs(-1000);
+    //s4.setTargetAbs(-3000);
     g1.move();
     delay(100);
 
     s1.setTargetAbs(0);
-    StepperGroup({s1, s2}).move();
+    //s3.setTargetAbs(0);
+    //StepperGroup({s1, s2, s3, s4}).move();
+    StepperGroup({s1, s2, s3, s4}).move();
 }
 
 void loop()
