9d20910d6e539eb8abbd27f9d2c571069e2d40fc
HackinSpock <winchell587matthew@gmail.com>
2024-10-02 12:03:08 -0400

    small revision in rotate() to remove while loop for testing

--- src/steppergroup.h
+++ src/steppergroup.h
@@ -58,17 +58,17 @@ namespace TS4
         void rotate()
         {
             startRotate(); // start movement in the background
-            while (1)    // wait until all steppers have stopped
-            {
-                delay(1);
-                bool done = true;
-                for (auto stepper : steppers)
-                {
-                    if (stepper->isMoving)
-                        done = false;
-                }
-                if (done) break;
-            }
+            // while (1)    // wait until all steppers have stopped
+            // {
+            //     delay(1);
+            //     bool done = true;
+            //     for (auto stepper : steppers)
+            //     {
+            //         if (stepper->isMoving)
+            //             done = false;
+            //     }
+            //     if (done) break;
+            // }
         }
 
         // void rotateAsync(int32_t v1, int32_t v2)
